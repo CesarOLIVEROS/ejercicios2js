e304169f361773ce1c2018fe3a14e7b8cd96f2c7
David <cesaroliveros23@gmail.com>
2022-03-25 21:10:33 -0500

    Version con ingresos dinamicos

--- AppPresupuesto/js/app.js
+++ AppPresupuesto/js/app.js
@@ -11,7 +11,8 @@ const egresos = [
 // Se cargaran en un inicio estas dos funciones para lo cual se definen en el body con la funcion onload
 let cargarApp = () =>{
     cargarCabecero();
-
+    cargarIngresos();
+    cargarEgresos();
 }
 
 // Calculamos la cantidad de ingresos obtenidos del arreglo
@@ -54,7 +55,68 @@ const formatoPorcentaje = (valor) =>{
     return valor.toLocaleString('en-US', {style:'percent',minimumFractionDigits:2});
 }
 
+const cargarIngresos=()=>{
+    let ingresosHTML ='';
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
 
+const crearIngresoHTML=(ingreso)=>{
+    let IngresoHTML = `
+    <div class="elemento limpiarEstilos" >
+    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>
+        <div class="elemento_eliminar">
+            <button class="elemento_eliminar--btn">
+                <ion-icon name="close-circle-outline"
+                onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
+            </button>
+        </div>
+    </div>
+    </div>
+    `;
+    return IngresoHTML;
 
+}
 
+// al buscar el id con findIndex se le pasa el atributo a buscar por parametro y luego se le pasa la funcion fecha para
+// determinar la igualdad y asi luego de la validacion se devuelve el indice del valor a buscar.
+const eliminarIngreso = (id)=>{
+    // con el valor ingreso se itera el arreglo de ingresos 
+    let indiceEliminar = ingresos.findIndex(ingreso=> ingreso.id === id);
+    ingresos.splice(indiceEliminar, 1);
+    //luego de indicarle cuantos indices borrar se vuelve a cargar los datos de la pagina para refrescar la informacion.
+    cargarCabecero();
+    cargarIngresos();
+}
 
+const cargarEgresos=()=>{
+    let EgresosHTML ='';
+    for(let egreso of egresos){
+        EgresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = EgresosHTML;
+
+}
+
+const crearEgresoHTML =(egreso)=>{
+    let EgresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor"> ${formatoMoneda(egreso.valor)}</div>
+        
+            <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
+                <div class="elemento_eliminar">
+                <button class="elemento_eliminar--btn">
+                    <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+    return EgresoHTML;
+}
